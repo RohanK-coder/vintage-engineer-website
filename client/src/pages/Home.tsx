3c26fdc857045863708daea691eaff5688698f96
RohanK-coder <kyrohan7730@gmail.com>
2025-07-10 00:40:43 +0530

    navigation-bar added

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+
+export default function Home() {
+  return (
+    <div>
+      <ResponsiveAppBar/>
+    </div>
+  )
+}
